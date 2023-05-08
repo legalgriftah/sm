04499425a62595d77443db334ee1eca9f05272bb
legalgriftah <halofhentai@gmail.com>
2023-05-08 12:23:33 +0200

    m changes

--- js/slotmachineUtils.js
+++ js/slotmachineUtils.js
@@ -102,8 +102,40 @@ function calcPlayerStats (){
     player.setCredits = player.getCredits -3;
     player.setSpins = player.getCredits / 3;
     player.setPlayed = player.getPlayed + 1;
+    player.setPayed = player.getPayed  + 3;
 }
-
+function remove () {
+    var e = document.querySelector('ul#first');
+    var child = e.lastElementChild; 
+    while (child) {
+        e.removeChild(child);
+        child = e.lastElementChild;
+    }
+    var e = document.querySelector('ul#second');
+    var child = e.lastElementChild; 
+    while (child) {
+        e.removeChild(child);
+        child = e.lastElementChild;
+    }
+    var e = document.querySelector('ul#third');
+    var child = e.lastElementChild; 
+    while (child) {
+        e.removeChild(child);
+        child = e.lastElementChild;
+    }
+    var e = document.querySelector('ul#forth');
+    var child = e.lastElementChild; 
+    while (child) {
+        e.removeChild(child);
+        child = e.lastElementChild;
+    }
+    var e = document.querySelector('ul#fifth');
+    var child = e.lastElementChild; 
+    while (child) {
+        e.removeChild(child);
+        child = e.lastElementChild;
+    }
+}
 function randomize (){
     var ul = document.querySelector('ul#first');
     for (var i = ul.children.length; i >= 0; i--) {
@@ -129,11 +161,8 @@ function randomize (){
 
 function updateText (){
     var paragraph = document.getElementById("credits");
-        var paragraph2 = document.getElementById("money");
         var paragraph3 = document.getElementById("spins");
         var paragraph4 = document.getElementById("jackpot");
-
-        paragraph2.textContent =  money;
         paragraph4.textContent = 'Jackpot';
         paragraph.textContent =  player.getCredits;
         paragraph3.textContent =  player.getSpins;
@@ -168,37 +197,45 @@ function getRandom(min, max) {
 }
 
 function calcSpin(){
-    arrayImgs = [];
-    arrayImgs.push("img/slot/banana.png");
-    arrayImgs.push("img/slot/coco.png");
-    arrayImgs.push("img/slot/setas.png");
-    arrayImgs.push("img/slot/k.png");
-    arrayImgs.push("img/slot/melocoton.png");
-    arrayImgs.push("img/slot/pina.png");
-    arrayImgs.push("img/slot/cofrewild.png");
+    var percentPays = ((player.getWinned / player.payed) * 100);
+    console.log((player.getWinned / player.payed) * 100);
+    if(percentPays < 50){
+        arrayImgs = [];
+        arrayImgs.push("img/slot/banana.png");
+        arrayImgs.push("img/slot/coco.png");
+        arrayImgs.push("img/slot/setas.png");
+        arrayImgs.push("img/slot/k.png");
+        arrayImgs.push("img/slot/melocoton.png");
+        arrayImgs.push("img/slot/pina.png");
+        arrayImgs.push("img/slot/cofrewild.png");
 
-    slot1Pos1 = document.querySelector('ul#first').getElementsByTagName("li")[3];
-    slot2Pos1 = document.querySelector('ul#second').getElementsByTagName("li")[3];
-    slot3Pos1 = document.querySelector('ul#third').getElementsByTagName("li")[3];
-    slot4Pos1 = document.querySelector('ul#forth').getElementsByTagName("li")[3];
-    slot5Pos1 = document.querySelector('ul#fifth').getElementsByTagName("li")[3];
-    
-    randomPos = Number(getRandom(0, arrayImgs.length-1));
+        slot1Pos1 = document.querySelector('ul#first').getElementsByTagName("li")[3];
+        slot2Pos1 = document.querySelector('ul#second').getElementsByTagName("li")[3];
+        slot3Pos1 = document.querySelector('ul#third').getElementsByTagName("li")[3];
+        slot4Pos1 = document.querySelector('ul#forth').getElementsByTagName("li")[3];
+        slot5Pos1 = document.querySelector('ul#fifth').getElementsByTagName("li")[3];
+        
+        randomPos = Number(getRandom(0, arrayImgs.length-1));
 
-    slot1Pos1.id = arrayImgs.length-1;
-    slot1Pos1.firstChild.setAttribute("src", arrayImgs[randomPos]);
+        slot1Pos1.id = arrayImgs.length-1;
+        slot1Pos1.firstChild.setAttribute("src", arrayImgs[randomPos]);
+        
+        slot2Pos1.id = arrayImgs.length-1;
+        slot2Pos1.firstChild.setAttribute("src", arrayImgs[randomPos]);
+        
+        slot3Pos1.id = arrayImgs.length-1;
+        slot3Pos1.firstChild.setAttribute("src", arrayImgs[randomPos]);
+        
+        slot4Pos1.id = arrayImgs.length-1;
+        slot4Pos1.firstChild.setAttribute("src", arrayImgs[randomPos]);
+        
+        slot5Pos1.id = arrayImgs.length-1;
+        slot5Pos1.firstChild.setAttribute("src", arrayImgs[randomPos]);
+    }else{
+        randomize();
+    }
+
     
-    slot2Pos1.id = arrayImgs.length-1;
-    slot2Pos1.firstChild.setAttribute("src", arrayImgs[randomPos]);
-    
-    slot3Pos1.id = arrayImgs.length-1;
-    slot3Pos1.firstChild.setAttribute("src", arrayImgs[randomPos]);
-    
-    slot4Pos1.id = arrayImgs.length-1;
-    slot4Pos1.firstChild.setAttribute("src", arrayImgs[randomPos]);
-    
-    slot5Pos1.id = arrayImgs.length-1;
-    slot5Pos1.firstChild.setAttribute("src", arrayImgs[randomPos]);
 
     //ELIMINAR EL ELEGIDO PARA EVITAR DUPLICADOS? estudiar comportamiento para corregir la deteccion de los premios
 }
@@ -241,6 +278,9 @@ function calcPrizes(){
         //slot3Pos3.className = "slotWin";
         slot4Pos3.className = "slot";
         slot5Pos3.className = "slot";
+        
+        player.setWinned = Number(Number(player.getWinned) + Number(slot1Pos1.id));
+        player.setCredits = Number(Number(player.getCredits) +  Number(slot1Pos1.id));
     }
     
     if(slot1Pos1.id == slot2Pos1.id && slot2Pos1.id == slot3Pos1.id && slot3Pos1.id == slot4Pos1.id && slot4Pos1.id == slot5Pos1.id){
@@ -262,7 +302,8 @@ function calcPrizes(){
         slot3Pos3.className = "slot";
         slot4Pos3.className = "slot";
         slot5Pos3.className = "slot";
-        player.setWinned = player.getWinned + 1;
+        player.setWinned = Number(Number(player.getWinned) + Number(slot1Pos1.id));
+        player.setCredits = Number(Number(player.getCredits) +  Number(slot1Pos1.id));
     }
     if(slot1Pos2.id == slot2Pos2.id && slot2Pos2.id == slot3Pos2.id && slot3Pos2.id == slot4Pos2.id && slot4Pos2.id == slot5Pos2.id){
         console.log('premio segunda fila');
@@ -283,6 +324,9 @@ function calcPrizes(){
         slot3Pos3.className = "slot";
         slot4Pos3.className = "slot";
         slot5Pos3.className = "slot";
+
+        player.setWinned = Number(Number(player.getWinned) + Number(slot2Pos1.id));
+        player.setCredits = Number(Number(player.getCredits) +  Number(slot2Pos1.id));
     }
 
     if(slot1Pos3.id == slot2Pos3.id && slot2Pos3.id == slot3Pos3.id && slot3Pos3.id == slot4Pos3.id && slot4Pos3.id == slot5Pos3.id){
@@ -304,6 +348,8 @@ function calcPrizes(){
         slot3Pos3.className = "slotWin";
         slot4Pos3.className = "slotWin";
         slot5Pos3.className = "slotWin";*/
+        player.setWinned = Number(Number(player.getWinned) + Number(slot3Pos1.id));
+        player.setCredits = Number(Number(player.getCredits) +  Number(slot3Pos1.id));
     }
     if(slot1Pos1.id == slot2Pos1.id && slot2Pos1.id == slot3Pos2.id && slot3Pos2.id == slot4Pos1.id && slot4Pos1.id == slot5Pos1.id){
         console.log('v pequeña');
@@ -324,6 +370,9 @@ function calcPrizes(){
         slot3Pos3.className = "slot";
         slot4Pos3.className = "slot";
         slot5Pos3.className = "slot";
+        
+        player.setWinned = Number(Number(player.getWinned) + Number(slot1Pos1.id));
+        player.setCredits = Number(Number(player.getCredits) +  Number(slot1Pos1.id));
     }
 
     if(slot1Pos2.id == slot2Pos1.id && slot2Pos1.id == slot3Pos1.id && slot3Pos1.id == slot4Pos1.id && slot4Pos1.id == slot5Pos2.id){
@@ -345,6 +394,9 @@ function calcPrizes(){
         slot3Pos3.className = "slot";
         slot4Pos3.className = "slot";
         slot5Pos3.className = "slot";
+
+        player.setWinned = Number(Number(player.getWinned) + Number(slot1Pos2.id));
+        player.setCredits = Number(Number(player.getCredits) +  Number(slot1Pos2.id));
     }
 
     if(slot1Pos2.id == slot2Pos1.id && slot2Pos1.id == slot3Pos2.id && slot3Pos2.id == slot4Pos1.id && slot4Pos1.id == slot5Pos2.id){
@@ -366,6 +418,9 @@ function calcPrizes(){
         slot3Pos3.className = "slot";
         slot4Pos3.className = "slot";
         slot5Pos3.className = "slot";
+
+        player.setWinned = Number(Number(player.getWinned) + Number(slot1Pos2.id));
+        player.setCredits = Number(Number(player.getCredits) +  Number(slot1Pos2.id));
     }
 
     if(slot1Pos2.id == slot2Pos3.id && slot2Pos3.id == slot3Pos3.id && slot3Pos3.id == slot4Pos3.id && slot4Pos3.id == slot5Pos2.id){
@@ -387,6 +442,9 @@ function calcPrizes(){
        // slot3Pos3.className = "slotWin";
        // slot4Pos3.className = "slotWin";
         slot5Pos3.className = "slot";
+
+        player.setWinned = Number(Number(player.getWinned) + Number(slot1Pos2.id));
+        player.setCredits = Number(Number(player.getCredits) +  Number(slot1Pos2.id));
     }
 
     if(slot1Pos3.id == slot2Pos3.id && slot2Pos3.id == slot3Pos2.id && slot3Pos2.id == slot4Pos3.id && slot4Pos3.id == slot5Pos3.id){
@@ -408,6 +466,9 @@ function calcPrizes(){
         slot3Pos3.className = "slot";
        // slot4Pos3.className = "slotWin";
        // slot5Pos3.className = "slotWin";
+
+       player.setWinned = Number(Number(player.getWinned) + Number(slot1Pos3.id));
+       player.setCredits = Number(Number(player.getCredits) +  Number(slot1Pos3.id));
     }
 
     if(slot1Pos3.id == slot2Pos2.id && slot2Pos2.id == slot3Pos1.id && slot3Pos1.id == slot4Pos2.id && slot4Pos2.id == slot5Pos3.id){
@@ -429,21 +490,20 @@ function calcPrizes(){
         slot3Pos3.className = "slot";
         slot4Pos3.className = "slot";
        // slot5Pos3.className = "slotWin";
+
+       player.setWinned = Number(Number(player.getWinned) + Number(slot1Pos3.id));
+       player.setCredits = Number(Number(player.getCredits) +  Number(slot1Pos3.id));
     }
 }
 
 const player = {
     credits : 60,
-    money : 0,
     spins : credits/3,
     winned : 0,
     payed: 0,
     played: 0,
     set setCredits(credits) {
         this.credits = credits;
-    },
-    set setMoney(money){
-        this.money = money;
     },
     set setSpins(spins){
         this.spins = spins;
@@ -459,9 +519,6 @@ const player = {
     },
     get getCredits() {
         return this.credits;
-    },
-    get getMoney() {
-        return this.money;
     },
     get getSpins(){
         return this.spins;
